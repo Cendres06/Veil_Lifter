6bf95b14eed1597099c323a7b9c19c9f4153f037
Cendres06 <30781127+Cendres06@users.noreply.github.com>
2022-06-22 11:19:44 +0200

    Upload de la V1 du Veil Lifter

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='veil_lifter',
+      version='0.1',
+      description='Module python servant à récupérer les textes de la branche francophone et à effectuer plusieurs opérations statistiques.',
+      url='https://github.com/Cendres06/Veil_Lifter',
+      author='Perrine MAUREL',
+      author_email='Secret',
+      license='GPL-3.0 license',
+      packages=['veil_lifter'],
+      zip_safe=True)
