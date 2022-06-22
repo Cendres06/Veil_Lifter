1d99f423967f34d62983704aa9fd9ad5ad91c6ff
Cendres06 <30781127+Cendres06@users.noreply.github.com>
2022-06-22 14:43:21 +0200

    V2.0
    Amélioration des performances de scrapping du module de crédit :
    1. Ajout de fonction "nettoyage_profondeur()" pour nettoyer l'HTML, option pour garder les liens
    2. Ajout de fonction "champs_ok()" pour détecter si une chaîne peut être assimilée à un champ (imparfait)
    3. Plus de cas particuliers pour améliorer le parsing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='veil_lifter',
-      version='0.1',
+      version='0.2',
       description='Module python servant à récupérer les textes de la branche francophone et à effectuer plusieurs opérations statistiques.',
       url='https://github.com/Cendres06/Veil_Lifter',
       author='Perrine MAUREL',
